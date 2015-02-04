2c92051b0bd6eb4c3011e6918367cc5324d47ffc
Matt Kennedy <Matt.Kenn3dy@gmail.com>
2015-02-03 21:01:46 -0500

    Band
    Checkout Page Done

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Band.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Instruments()
+        {
+            ViewBag.Message = "Your Instruments description page.";
+
+            return View();
+        }
+
+
+    }
+}
